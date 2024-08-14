51620cb1b4e7dd356309b6adc0a53a1b93c7c83b
Anas Almasri <Anaskhaldoun2@gmail.com>
2024-08-14 00:31:34 -0400

    Added a photo to the hero plus other minor fixes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,14 +14,16 @@ const About = () => {
   }, []);
 
   return (
-    <Flex id="about" flexDir="column" w="100%" data-aos="fade-up">
+    <Flex id="about" flexDir="column" w="100%">
       <Flex flexDir={{base:"column", md:"row"}} margin="20px" alignItems="center" justifyContent="center">
         <Text fontSize="3xl" className='title'>About</Text>
         <Divider w={{base:"90%", md:"50%"}} ml={{base:0, md:"1em"}} bgColor="teal.300" h="2.5px" borderRadius="10px"/>
       </Flex>
-      <Flex justifyContent="space-around">
-        <Text fontSize={"xl"} textAlign="center" w={{base:"90%", md:"55%"}}>
-          I am currently attending the University of Toronto where I am studying Computer Science. I always strive to learn new technologies and skills and to keep up with the ever-changing world. I am passionate about software development and I am always looking for new opportunities to grow and learn. Some of my current skills include Python, JavaScript, and React.
+      <Flex justifyContent="space-around" data-aos="fade-up">
+        <Text fontSize={{base:"xl", lg:"3xl"}} textAlign="center" w={{base:"90%", md:"55%"}}>
+          I am currently attending the University of Toronto where I am studying Computer Science. 
+          <br/><br/>I always strive to learn new technologies, and skills and to keep up with the ever-changing world. I am passionate about software development, and I am always looking for new opportunities to grow and learn. 
+          <br/><br/> My current skills include Python, JavaScript, and React.
         </Text>
       </Flex>
     </Flex>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,7 +5,13 @@ import {
   Text,
   Button,
   Stack,
+  Flex,
+  Image,
+  Icon,
 } from "@chakra-ui/react";
+import { FaGithubSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
 
 export default function Profile({ color }) {
   // const scrollToContact = () => {
@@ -28,20 +34,23 @@ export default function Profile({ color }) {
         />
       </Heading>
 
-      <Container maxW={"3xl"} id="profile">
+      <Flex flexDir={{base:"column", lg:"row"}} w="100%" alignItems="center" id="profile">
         <Stack
           as={Box}
+          maxW={"3xl"}
           textAlign={"center"}
           spacing={{ base: 8, md: 14 }}
-          pb={{ base: '33vh', md: '35vh' }}
-          pt={{ base: '33vh', md: '23vh' }}
+          pb={{ base: '10vh', lg: '35vh' }}
+          pt={{ base: '14vh', md: '23vh' }}
+          mr="auto"
+          m="auto"
         >
           <Heading
             fontWeight={600}
             fontSize={{ base: "6xl", sm: "80px", md: "96px" }}
             lineHeight={"110%"}
           >
-            {'Hi my name is'} <br />
+            {'Hi, my name is'} <br />
             <Text as={"span"} color={`teal.400`}>
               {"Anas Almasri"}
             </Text>
@@ -76,17 +85,23 @@ export default function Profile({ color }) {
             >
               Let's connect!
             </Button>
-            {/* <Button
-              variant={"link"}
-              colorScheme={"blue"}
-              size={"sm"}
-              onClick={scrollToContact}
-            >
-              Contact Me
-            </Button> */}
+            <Flex justifyContent="center" mt={10}>
+              <a href="https://github.com/anousalma" target='_blank' rel="noreferrer"><Icon as={FaGithubSquare} w={"2.5em"} h={"2.5em"} mr={10}/></a>
+              <a href="https://www.linkedin.com/in/anas-almasri-45491b298/" target='_blank' rel="noreferrer"><Icon as={FaLinkedin} w={"2.5em"} h={"2.5em"} mr={10}/></a>
+              <a href="mailto:anask.almasri@gmail.com" target="_blank" rel="noreferrer"><Icon as={IoMdMail} w={"2.5em"} h={"2.5em"}/></a>
+            </Flex>
           </Stack>
         </Stack>
-      </Container>
+        <Image 
+          src="https://i.ibb.co/f0D0KJd/nice-anas-photo.jpg" 
+          ml="auto" mr="auto" 
+          w="auto"
+          h="500px"
+          borderRadius="100%" border="20px" borderColor="black"
+          mb={{ base: '15vh', lg: '10vh' }}
+          flex="grow"
+          />
+      </Flex>
     </>
   );
 }
